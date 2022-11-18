84adee2fdf3bcdc34fa4479fc2a4dfd659f156cf
Ravindu Wegiriya <ravinduwegiriya@gmail.com>
2022-11-18 11:13:29 +0530

    Add PHP session maintenance for admin

--- admin/suspend-user.php
+++ admin/suspend-user.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+//if not logged in redirect to login page
+ if (!isset($_SESSION['username']) || $_SESSION['user_type'] != 'Admin') {
+      header("Location: admin-login.php"); 
+ }
     require_once '../db.php';
 
     //use the ajax fetch call to get the user id and do the query and return status
--- admin/users.php
+++ admin/users.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+   //if not logged in redirect to login page
+    if (!isset($_SESSION['username']) || $_SESSION['user_type'] != 'Admin') {
+         header("Location: admin-login.php"); 
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
