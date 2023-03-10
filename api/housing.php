8de54b399626f2b83375a0abaf4682aafa0cc1c8
Ravindu Wegiriya <ravinduwegiriya@gmail.com>
2023-03-10 17:47:47 +0530

    Update housing.php to automatically mark jobs as complete upon house creation

--- api/housing.php
+++ api/housing.php
@@ -10,10 +10,26 @@
         $jobSelection = $postData['jobselection'];
 
         $sql_create_housing = "INSERT INTO House(Customer_ID, Address, Verified) VALUES($customerId, '$address', $verified)";
+        $result_create_housing = $conn->query($sql_create_housing);
+
+        $sql_get_housing_id = "SELECT House_ID FROM House WHERE Customer_ID = $customerId AND Address = '$address'";
+        $result_get_housing_id = $conn->query($sql_get_housing_id);
+        $row = $result_get_housing_id->fetch_assoc();
+        $housingId = $row['House_ID'];
+
+
+        //Jobs are automatically created by Trigger. This is to update the selected set of jobs as completed.
+        foreach ($jobSelection as $jobId) {
+            $sql_update_jobs = "UPDATE Job SET Completion_Flag = 1 WHERE Job_Type_ID = $jobId AND House_ID = $housingId";
+            $result_update_jobs = $conn->query($sql_update_jobs);
+
+        }
+
+
 
         $http_response_code = 0;
 
-        if($conn->query($sql_create_housing)){
+        if($result_create_housing){
             $http_response_code = 201; // Resource created
         } else {
             $http_response_code = 500;
