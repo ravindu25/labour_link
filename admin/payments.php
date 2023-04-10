7b96ebbd7b4df68f16c7faba90750978eb2ae805
Rushdha <rushdha2021@gmail.com>
2023-04-11 03:20:25 +0530

    admin-payments page

---
+++ admin/payments.php
@@ -0,0 +1,243 @@
+<?php
+session_start();
+//if not logged in redirect to login page
+if (!isset($_SESSION['username']) || $_SESSION['user_type'] != 'Admin') {
+    header("Location: admin-login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Google fonts imports -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&family=Poppins:wght@400;500;600&display=swap"
+          rel="stylesheet">
+
+    <!--Fontawesome-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet"/>
+
+    <!-- CSS files -->
+    <link href="../styles/index-page.css" rel="stylesheet"/>
+    <link href="../styles/dashboard.css" rel="stylesheet"/>
+    <link href="../styles/admin/payments.css" rel="stylesheet"/>
+    <title>Payments | Admin Dashboard</title>
+</head>
+<body>
+<div class="backdrop-modal" id="backdrop-modal">
+</div>
+<div class="backdrop-modal" id="admin-backdrop-modal">
+</div>
+<div class="success-message-container" id="register-success">
+    <h1><i class="fa-solid fa-check"></i></h1>
+    <div class="success-message-text">
+        <h1>Admin Account Created!</h1>
+    </div>
+</div>
+<div class="failed-message-container" id="register-failed">
+    <h1><i class="fa-solid fa-xmark"></i></h1>
+    <div class="failed-message-text">
+        <h1>Account Creation Failed</h1>
+        <h3 id="error-text"></h3>
+    </div>
+</div>
+<nav class="nav-bar">
+    <div class="nav-bar-items">
+        <div class="logo-container">
+            <img src="../assets/logo-croped.png" alt="labourlink logo" class="labour-link-logo"/>
+        </div>
+        <div class="search-container">
+            <div class="search-icon-container">
+                <img src="../assets/svg/search.svg" alt="search" class="search-icon"/>
+            </div>
+            <input type="text" class="search-bar-input" placeholder="Search for a labourer or a service"/>
+        </div>
+        <div class="nav-link-container">
+            <div class="nav-link-items"><a href="../index.php" class="nav-links">Home</a></div>
+            <div class="nav-link-items"><a href="../about-us.php" class="nav-links">About</a></div>
+            <div class="nav-link-items"><a href="../contact-us.php" class="nav-links">Contact Us</a></div>
+            <?php
+            if (!isset($_SESSION['username'])) {
+
+                ?>
+                <div class="nav-link-items">
+                    <button type="button" id="register-button" class="nav-link-items-button"
+                            style="background-color: #FFF; color: #102699;">
+                        REGISTER
+                    </button>
+                </div>
+                <div class="nav-link-items">
+                    <button type="button" class="nav-link-items-button" onclick="window.location.href='login.php'">
+                        LOGIN
+                    </button>
+                </div>
+            <?php }else{ ?>
+                <div class="nav-link-items">
+                    <div class="dropdown" id="dropdown">
+                        <button type="button" id="user-dropdown-button" onClick="opendropdown()"
+                                class="nav-link-items-button"
+                                style="background-color: #FFF; color: #102699;">
+                            <i class="fa-regular fa-circle-user"></i>&nbsp;
+                            Hi,&nbsp;<?php echo $_SESSION['first_name']; ?>
+                            &nbsp;
+                            <i class="fa-solid fa-chevron-down"></i>
+                        </button>
+                        <div class="dropdown-items" id="dropdown-items">
+                            <a href="#">
+                                <div class="dropdown-item" id="dropdown-item"><i class="fa-solid fa-gauge-high"></i>&nbsp;&nbsp;Dashboard
+                                </div>
+                            </a>
+                            <a href="#">
+                                <div class="dropdown-item" id="dropdown-item">
+                                    <i class="fa-solid fa-right-from-bracket"></i>
+                                    &nbsp;&nbsp;<a href="../logout.php">Logout</a>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</nav>
+<main class="main-section">
+    <section class="sidebar">
+        <h1 class="sidebar-heading">Dashboard</h1>
+        <div class="sidebar-items">
+            <a href="./dashboard.php">
+                <div class="sidebar-item">
+                    <i class="fa-solid fa-server sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Overview</h4>
+                </div>
+            </a>
+            <a href="./bookings.php">
+                <div class="sidebar-item">
+                    <i class="fa-solid fa-b sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Booking</h4>
+                </div>
+            </a>
+            <a href="./feedbacks.php">
+                <div class="sidebar-item">
+                    <i class="fa-regular fa-message sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Feedbacks</h4>
+                </div>
+            </a>
+            <a href="./housing.php">
+                <div class="sidebar-item">
+                    <i class="fa-solid fa-house sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Housing</h4>
+                </div>
+            </a>
+            <a href="./payments.php">
+                <div class="sidebar-item sidebar-item-selected">
+                    <i class="fa-regular fa-credit-card sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Payments</h4>
+                </div>
+            </a>
+            <a href="./users.php">
+                <div class="sidebar-item">
+                    <i class="fa-solid fa-user-check sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Users</h4>
+                </div>
+            </a>
+            <a href="./reports.php">
+                <div class="sidebar-item">
+                    <i class="fa-regular fa-newspaper sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Reports</h4>
+                </div>
+            </a>
+            <a href="./profile.php">
+                <div class="sidebar-item">
+                    <i class="fa-regular fa-circle-user sidebar-item-icon"></i>
+                    <h4 class="sidebar-icon-heading">Profile</h4>
+                </div>
+            </a>
+        </div>
+    </section>
+    <section class="main-content">
+        <div class="loader-container" id="loader-container">
+            <svg id="spinner" class="spinner" width="50%" height="50%" viewBox="0 0 50 50">
+                <circle class="path" cx="25" cy="25" r="20" fill="#ABC3EF" stroke-width="5"></circle>
+            </svg>
+        </div>
+        <div class="main-content-container" id="main-content-container">
+            <div class="main-heading">
+                <h1>Control panel for managing <u>Payments</u></h1>
+                <h5>Logged as <?php echo $_SESSION['first_name'] . " " . $_SESSION['last_name'] ?></h5>
+            </div>
+        </div>
+        <div class="due-payments">
+            <div class="due-payments-title">
+                <h1>Due Payments</h1>
+            </div>
+            <div class="due-payments-container">
+                <table class="main-table">
+                    <thead>
+                    <tr class="main-tr">
+                        <th class="main-th">Customer Name</th>
+                        <th class="main-th">Worker Name</th>
+                        <th class="main-th">Start Date</th>
+                        <th class="main-th">Amount</th>
+                        <th class="main-th">Payment Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr class="main-tr">
+                        <td class="main-td" style="text-align: left;">Saman Gunawardhana
+                            <br/>
+                        </td>
+                        <td class="main-td">Sunil Perera</td>
+                        <td class="main-td">19 Nov 2022</td>
+                        <td class="main-td">Rs. 27000.00</td>
+                        <td class="main-td">Success</td>
+                    </tr>
+                    <tr class="main-tr">
+                        <td class="main-td" style="text-align: left;">Avinash Sudira
+                            <br/>
+                        </td>
+                        <td class="main-td">Sunil Perera</td>
+                        <td class="main-td">30 Nov 2022</td>
+                        <td class="main-td">Rs. 12500.00</td>
+                        <td class="main-td">Failed</td>
+                    </tr>
+                    <tr class="main-tr">
+                        <td class="main-td" style="text-align: left;">Dinesh Attanayaka
+                            <br/>
+                        </td>
+                        <td class="main-td">Sunil Perera</td>
+                        <td class="main-td">01 Nov 2022</td>
+                        <td class="main-td">Rs. 1700.00</td>
+                        <td class="main-td">Success</td>
+                    </tr>
+                    <tr class="main-tr">
+                        <td class="main-td" style="text-align: left;">Kapila Dharmadhasa
+                            <br/>
+                        </td>
+                        <td class="main-td">Sunil Perera</td>
+                        <td class="main-td">19 Nov 2022</td>
+                        <td class="main-td">Rs. 12000.00</td>
+                        <td class="main-td">Success</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- Search for payments section -->
+        <div class="booking-search">
+            <div class="booking-search-title">
+                <h1>Search for payments</h1>
+                <form action="" method="POST">
+                    <div class="booking-search-input-container">
+                        <label for="booking-search">Search (Using customer name, date etc)</label>
+                        <div class="booking-search-input-field">
+                            <input type="text" id="booking-search" class="booking-search-input" name="users-search"/>
+                            <button class="search-icon-small"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</main>
