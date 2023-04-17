a6857128d173a764df30bba9037b7efbb0121712
Mohamed Izzath <41626585+Mohamedizzath@users.noreply.github.com>
2023-04-17 12:49:07 +0530

    Added fetching worker category

--- api/workers.php
+++ api/workers.php
@@ -47,8 +47,9 @@
                     $address = $row['User_Address'];
                     $city = $row['City'];
                     $currentRating = $row['Current_Rating'];
+                    $workerCategories = getAllCategories($conn, $userId);
 
-                    $worker = new Worker($userId, $fullName, $email, $contactNum, $nic, $dob, $address, $city, $currentRating);
+                    $worker = new Worker($userId, $fullName, $email, $contactNum, $nic, $dob, $address, $city, $currentRating, $workerCategories);
 
                     array_push($workers, $worker);
                 }
@@ -57,4 +58,59 @@
             header('Content-Type: application/json');
             echo json_encode($workers);
         }
+    }
+
+    function getAllCategories($conn, $workerId){
+        $sql_statement = "SELECT Worker.Worker_ID, P.Plumber_ID, C.Carpenter_ID, E.Electrician_ID, P2.Painter_ID,
+        M.Mason_ID, J.Janitor_ID, M2.Mechanic_ID, Gardener_ID
+                    FROM Worker LEFT JOIN Plumber P on Worker.Worker_ID = P.Plumber_ID
+                    LEFT JOIN Carpenter C on Worker.Worker_ID = C.Carpenter_ID
+                    LEFT JOIN Electrician E on Worker.Worker_ID = E.Electrician_ID
+                    LEFT JOIN Painter P2 on Worker.Worker_ID = P2.Painter_ID
+                    LEFT JOIN Mason M on Worker.Worker_ID = M.Mason_ID
+                    LEFT JOIN Janitor J on Worker.Worker_ID = J.Janitor_ID
+                    LEFT JOIN Mechanic M2 on Worker.Worker_ID = M2.Mechanic_ID
+                    LEFT JOIN Gardener G on Worker.Worker_ID = G.Gardener_ID WHERE Worker_ID = $workerId";
+
+        $result = $conn->query($sql_statement);
+
+        $workerCategories = array();
+
+        if($result->num_rows){
+            while($row = $result->fetch_assoc()){
+                if($row['Plumber_ID'] != null){
+                    array_push($workerCategories, "Plumber");
+                }
+
+                if($row['Carpenter_ID'] != null){
+                    array_push($workerCategories, "Carpenter");
+                }
+
+                if($row['Electrician_ID'] != null){
+                    array_push($workerCategories, "Electrician");
+                }
+
+                if($row['Painter_ID'] != null){
+                    array_push($workerCategories, "Painter");
+                }
+
+                if($row['Mason_ID'] != null){
+                    array_push($workerCategories, "Mason");
+                }
+
+                if($row['Janitor_ID'] != null){
+                    array_push($workerCategories, "Janitor");
+                }
+
+                if($row['Mechanic_ID'] != null){
+                    array_push($workerCategories, "Mechanic");
+                }
+
+                if($row['Gardener_ID'] != null){
+                    array_push($workerCategories, "Gardener");
+                }
+            }
+        }
+
+        return $workerCategories;
     }
