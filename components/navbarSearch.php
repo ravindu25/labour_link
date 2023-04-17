616fc614921acead9e69ecd4a083b95c422cfeee
Mohamed Izzath <41626585+Mohamedizzath@users.noreply.github.com>
2023-04-17 21:20:38 +0530

    Added navbar search component

--- components/navbarSearch.php
+++ components/navbarSearch.php
@@ -1,7 +1,7 @@
 <?php
     require_once "../db.php";
     $searchInput = $_POST['searchInput'];
-    $sql = "SELECT First_Name, Last_Name FROM User WHERE First_Name LIKE '%$searchInput%' OR Last_Name LIKE '%$searchInput%'";
+    $sql = "SELECT UserID, First_Name, Last_Name FROM User WHERE First_Name LIKE '%$searchInput%' OR Last_Name LIKE '%$searchInput%'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
