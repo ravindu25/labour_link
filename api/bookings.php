92faeed79898b9ca0ad0edc814e9615466afd6e7
Rushdha <rushdha2021@gmail.com>
2023-04-27 12:31:09 +0530

    update api/bookings

--- api/bookings.php
+++ api/bookings.php
@@ -77,6 +77,46 @@
             header('Content-Type: application/json');
             echo json_encode($all_bookings);
         }
+        else if (isset($_GET['workerId'])) {
+            $workerId = $_GET['workerId'];
+            // Getting all the bookings
+            $sql_get_all_bookings = "select Booking.*, Worker.First_Name AS Worker_First_Name, Worker.Last_Name AS Worker_Last_Name, Customer.First_Name AS Customer_First_Name, Customer.Last_Name AS Customer_Last_Name from Booking inner join User AS Worker ON Booking.Worker_ID = Worker.User_ID inner join User AS Worker ON Booking.Worker_ID = Worker.User_ID where Booking.Worker_ID = $workerId ORDER BY Booking.Created_Date DESC";
+
+            // Check whether bookings amount is defined
+            if(isset($_GET['num'])){
+                $numOfBookings = $_GET['num'];
+
+                $sql_get_all_bookings = $sql_get_all_bookings . " LIMIT $numOfBookings";
+            }
+
+            $result = $conn->query($sql_get_all_bookings);
+
+            $all_bookings = array();
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $bookingId = $row['Booking_ID'];
+                    $customerId = $row['Customer_ID'];
+                    $customerName = $row['Customer_First_Name'] . " " . $row['Customer_Last_Name'];
+                    $workerId = $row['Worker_ID'];
+                    $workerName = $row['Worker_First_Name'] . " " . $row['Worker_Last_Name'];
+                    $createdDate = $row['Created_Date'];
+                    $startDate = $row['Start_Date'];
+                    $endDate = $row['End_Date'];
+                    $workerType = $row['Worker_Type'];
+                    $status = $row['Status'];
+                    $paymentMethod = $row['Payment_Method'];
+
+                    $booking = new Booking($bookingId, $customerId, $customerName, $workerId, $workerName,
+                        $createdDate, $startDate, $endDate, $workerType, $status, $paymentMethod);
+
+                    array_push($all_bookings, $booking);
+                }
+            }
+
+            header('Content-Type: application/json');
+            echo json_encode($all_bookings);
+        }
     } else if($_SERVER['REQUEST_METHOD'] === 'DELETE' && $_GET['bookingId']){
         $bookingId = $_GET['bookingId'];
 
