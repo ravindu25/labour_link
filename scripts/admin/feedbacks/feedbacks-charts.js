dd57da3a535382af3fc243d11916f919a704f043
Mohamed Izzath <41626585+Mohamedizzath@users.noreply.github.com>
2023-05-03 22:19:52 +0530

    Added basic charts for admin/feedbacks

--- scripts/admin/feedbacks/feedbacks-charts.js
+++ scripts/admin/feedbacks/feedbacks-charts.js
@@ -1,26 +1,78 @@
 const allFeedbacksChart = document.getElementById('chart-all-feedbacks')
 
 
-function showFeedbackCount(allFeedbackCount) {
-    const xAxisLabels = allFeedbackCount.map(feedback => feedback.date);
-    const yAxisValues = allFeedbackCount.map(feedback => parseInt(feedback.feedbackCount));
+function showFeedbackCount(allFeedbackCount, year, month) {
+    /*
+     * Fill the array with number of days in the month
+     */
+    const numberOfDays = new Date(year, month, 0).getDate();
+    let xAxisLabels = [];
+    let yAxisValues = [];
 
-    console.log(allFeedbackCount);
+    for(let i = 1; i <= numberOfDays; i++){
+        const monthText = month < 10 ? `0${month}` : month;
+        const dayText = i < 10 ? `0${i}` : i;
+        xAxisLabels.push(`${year}-${monthText}-${dayText}`);
+
+        const element = allFeedbackCount.find(element => element.date === `${year}-${monthText}-${dayText}`);
+        if(element){
+            yAxisValues.push(element.feedbackCount);
+        } else {
+            yAxisValues.push(0);
+        }
+    }
 
     new Chart(allFeedbacksChart, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: xAxisLabels,
             datasets: [{
                 label: 'Number of Feedbacks',
+                fill: true,
+                backgroundColor: '#C4DDF2',
                 data: yAxisValues,
+                borderColor: '#102699',
                 borderWidth: 1
             }]
         },
         options: {
+            responsive: true,
             scales: {
                 y: {
-                    beginAtZero: true
+                    ticks: {
+                        stepSize: 1
+                    }
+                }
+            }
+        }
+    });
+}
+
+function showFeedbackSkippingRate(skippingRateData){
+    const skippingRateChart = document.getElementById('chart-feedback-skipping');
+    const xAxisLabels = skippingRateData.map(element => element.customerName);
+    const yAxisValues = skippingRateData.map(element => element.feedbackCount);
+
+    new Chart(skippingRateChart, {
+        type: 'bar',
+        data: {
+            labels: xAxisLabels,
+            datasets: [{
+                label: 'Number of skipped Feedbacks',
+                fill: true,
+                backgroundColor: '#F2D49B',
+                data: yAxisValues,
+                borderColor: '#FF5B19',
+                borderWidth: 2
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    ticks: {
+                        stepSize: 1
+                    }
                 }
             }
         }
--- scripts/admin/feedbacks/feedbacks.js
+++ scripts/admin/feedbacks/feedbacks.js
@@ -1,13 +1,34 @@
 let allFeedbackCount = null;
+let feedbackSkippingRate = null;
 
 function initialLoad(){
-    fetch(`http://localhost/labour_link/api/charts/feedbacks.php?term=getFeedbackCount`, {
+    const currentYear = new Date().getFullYear();
+    // const currentMonth = new Date().getMonth() + 1;
+    const currentMonth = 4;
+
+    fetch(`http://localhost/labour_link/api/charts/feedbacks.php?term=getFeedbackCount&year=${currentYear}&month=${currentMonth}`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
     }).then(response => response.json())
         .then(data => {
             allFeedbackCount = data;
-            showFeedbackCount(allFeedbackCount);
+            showFeedbackCount(allFeedbackCount, currentYear, currentMonth);
+
+            fetch(`http://localhost/labour_link/api/charts/feedbacks.php?term=getSkippingFeedback`, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+            }).then(response => response.json())
+                .then(data => {
+                    feedbackSkippingRate = data;
+                    showFeedbackSkippingRate(feedbackSkippingRate);
+                })
+                .catch(error => {
+                    const backdropModal = document.getElementById('backdrop-modal');
+                    const errorMessageContainer = document.getElementById('error-message-container');
+
+                    backdropModal.style.visibility = 'visible';
+                    errorMessageContainer.style.visibility = 'visible';
+                })
         })
         .catch(error => {
             const backdropModal = document.getElementById('backdrop-modal');
