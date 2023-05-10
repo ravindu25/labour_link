4992059ad468364cacb82c603d3960c395d9ea68
Mohamed Izzath <41626585+Mohamedizzath@users.noreply.github.com>
2023-05-10 22:01:02 +0530

    Updated booking creation according to the active workers

--- customer/get-workers.php
+++ customer/get-workers.php
@@ -6,16 +6,29 @@
 
     switch($workerType){
         case 'Electrician':
-            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Electrician on User.User_ID = Electrician.Electrician_ID";
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Electrician on User.User_ID = Electrician.Electrician_ID WHERE Electrician.Active = 1";
+            break;
+        case 'Plumber':
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Plumber on User.User_ID = Plumber.Plumber_ID WHERE Plumber.Active = 1";
             break;
         case 'Painter':
-            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Painter on User.User_ID = Painter.Painter_ID";
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Painter on User.User_ID = Painter.Painter_ID WHERE Painter.Active = 1";
+            break;
+        case 'Carpenter':
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Carpenter on User.User_ID = Carpenter.Carpenter_ID WHERE Carpenter.Active = 1";
             break;
         case 'Mason':
-            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Mason on User.User_ID = Mason.Mason_ID";
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Mason on User.User_ID = Mason.Mason_ID WHERE Mason.Active = 1";
             break;
-        default:
-            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Electrician on User.User_ID = Electrician.Electrician_ID";
+        case 'Janitor':
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Janitor on User.User_ID = Janitor.Janitor_ID WHERE Janitor.Active = 1";
+            break;
+        case 'Mechanical':
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Mechanic on User.User_ID = Mechanic.Mechanic_ID WHERE Mechanic.Active = 1";
+            break;
+        case 'Gardner':
+            $sql = "select User.User_ID, User.First_Name, User.Last_Name from User inner join Gardener on User.User_ID = Gardener.Gardener_ID WHERE Gardener.Active = 1";
+            break;
     }
 
     $result = $conn->query($sql);
